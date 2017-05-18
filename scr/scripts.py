798971fa33cfad57185e802a6075e25111523193
Saile <sailepradh@gmailcom>
2017-05-18 23:36:22 +0200

    break day

---
+++ scr/scripts.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import numpy.random as rdn
+
+# uncomment for line_profiler
+# @profile
+def test():
+    a = rdn.randn(100000)
+    b = np.repeat(a, 100)
+
+test()
